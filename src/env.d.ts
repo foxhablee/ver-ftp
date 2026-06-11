9690d6bc7f623f996f197fdd5e9370b7d72c6d3f
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-11 23:10:21 +0300

    chore: add test ftp docker compose

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_FTP_TEST_HOST: string
+    readonly VITE_FTP_TEST_PORT: string
+    readonly VITE_FTP_TEST_USER: string
+    readonly VITE_FTP_TEST_PASSWORD: string
+    readonly VITE_FTP_TEST_SECURE: 'true' | 'false'
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
