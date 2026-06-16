1d00ba2cc754bb47f0d0c98c215377c0a1551f9c
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-16 14:29:30 +0300

    feat(app/preload): auto type for IpcApi

--- src/app/preload/index.d.ts
+++ src/app/preload/index.d.ts
@@ -1,8 +1,9 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
+import { IpcApi } from './index'
 
 declare global {
     interface Window {
         electron: ElectronAPI
-        api: unknown
+        api: IpcApi
     }
 }
--- src/app/preload/index.ts
+++ src/app/preload/index.ts
@@ -3,8 +3,10 @@ import { contextBridge } from 'electron'
 // Custom APIs for renderer
 const api = {}
 
+export type IpcApi = typeof api
+
 try {
-  contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('api', api)
 } catch (error) {
-  console.error(error)
+    console.error(error)
 }
