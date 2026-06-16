13fb8f1894032e554e86ea22647fc55d9bdeb64e
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-16 14:50:46 +0300

    refactor(app/preload): remove index.d.ts file and move content to env.d.ts

--- src/app/preload/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
-import { IpcApi } from './index'
-
-declare global {
-    interface Window {
-        electron: ElectronAPI
-        api: IpcApi
-    }
-}
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,23 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-    readonly VITE_FTP_TEST_HOST: string
-    readonly VITE_FTP_TEST_PORT: string
-    readonly VITE_FTP_TEST_USER: string
-    readonly VITE_FTP_TEST_PASSWORD: string
-    readonly VITE_FTP_TEST_SECURE: 'true' | 'false'
+import { IpcApi } from '@/app/preload'
+
+declare global {
+    interface ImportMetaEnv {
+        readonly VITE_FTP_TEST_HOST: string
+        readonly VITE_FTP_TEST_PORT: string
+        readonly VITE_FTP_TEST_USER: string
+        readonly VITE_FTP_TEST_PASSWORD: string
+        readonly VITE_FTP_TEST_SECURE: 'true' | 'false'
+    }
+
+    interface ImportMeta {
+        readonly env: ImportMetaEnv
+    }
+
+    interface Window {
+        api: IpcApi
+    }
 }
 
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
+export {}
