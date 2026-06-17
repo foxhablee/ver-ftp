89d71d85b5b3a611d10791b2f2bda4564e18f9bf
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-17 20:25:49 +0300

    refactor(features/connect-to-ftp): create public api files according FSD

--- src/app/backend/ipc.ts
+++ src/app/backend/ipc.ts
@@ -1,6 +1,6 @@
 import { ipcMain } from 'electron'
 import { IPC } from '@/shared/ipc/preload'
-import { ipcConnectFTPHandler } from '@/features/connect-to-ftp/backend/ipcConnectFTPHandler'
+import { ipcConnectFTPHandler } from '@/features/connect-to-ftp/backend'
 
 export function registerIpcHandlers(): void {
     ipcMain.handle(IPC.FTP_CONNECT, ipcConnectFTPHandler)
