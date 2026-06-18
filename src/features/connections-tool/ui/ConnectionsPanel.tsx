8f9ff3d9edb79566c562f5afb7a4c1bbb0787ba3
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-18 12:49:42 +0300

    feat(features/connection-tool): button onClick handler for opening tool in subwindow

--- src/features/connections-tool/ui/ConnectionsPanel.tsx
+++ src/features/connections-tool/ui/ConnectionsPanel.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { Button } from '@mui/material'
 
 function ConnectionsPanel(): React.JSX.Element {
+    function clickHandler(): void {
+        window.api.window.create({ page: '/tool/connections' })
+    }
+
     return (
         <>
-            <Button variant='outlined'>Подключиться</Button>
+            <Button variant='outlined' type='button' onClick={clickHandler}>
+                Подключиться
+            </Button>
         </>
     )
 }
