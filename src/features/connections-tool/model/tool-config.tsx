e63b5ad3aefeb8333f9d4e79251d216a8c73eb6b
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-18 09:37:06 +0300

    feat(features/connections-tool): blank ConnectionsPanel

--- src/features/connections-tool/model/tool-config.tsx
+++ src/features/connections-tool/model/tool-config.tsx
@@ -1,11 +1,11 @@
-import { Fragment } from 'react'
 import { Tool } from '@/shared/model'
 import CableIcon from '@mui/icons-material/Cable'
+import ConnectionsPanel from '../ui/ConnectionsPanel'
 
 export const connectionsTool: Tool = {
     id: 'connections',
     title: 'Подключения',
     side: 'right',
-    Panel: Fragment,
+    Panel: ConnectionsPanel,
     icon: <CableIcon />,
 }
---
+++ src/features/connections-tool/ui/ConnectionsPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+function ConnectionsPanel(): React.JSX.Element {
+    return (
+        <>
+            <Button variant='outlined'>Подключиться</Button>
+        </>
+    )
+}
+
+export default ConnectionsPanel
