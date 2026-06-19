73de01571c92ea4c9af433e48e468bc0fba46069
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-20 00:07:25 +0300

    chore(linter): config steiger; add run configs

--- steiger.config.ts
+++ steiger.config.ts
@@ -4,13 +4,6 @@ import fsd from '@feature-sliced/steiger-plugin'
 export default defineConfig([
     ...fsd.configs.recommended,
     {
-        files: ['./src/renderer/**'],
-    },
-    {
-        files: ['./src/backend/**'],
-    },
-
-    {
         ignores: ['**/__mocks__/**', '**/*.test.*', '**/*.spec.*'],
     },
 ])
