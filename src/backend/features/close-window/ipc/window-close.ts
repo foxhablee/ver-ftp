e50bc21f1a220842ea411d77bbdf052c55c2eb90
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-23 10:37:07 +0300

    refactor(features/close-window): rewrite with new wrapIpcHandler and IpcError

--- src/backend/features/close-window/ipc/window-close.ts
+++ src/backend/features/close-window/ipc/window-close.ts
@@ -1,26 +1,18 @@
-import type { BrowserWindow, IpcMainInvokeEvent } from 'electron'
-import { IPC_METHODS, IpcResponseMap } from '@/shared/ipc'
+import type { BrowserWindow } from 'electron'
+import { IPC_METHODS, IpcError } from '@/shared/ipc'
 import type { IpcWindowCloseProps } from '@/shared/model'
 import { closeWindow } from '../api/close-window'
+import { wrapIpcHandler } from '@/backend/shared/lib'
 
 interface Deps {
     parent: BrowserWindow
 }
 
 export function ipcHandlerWindowClose(deps: Deps) {
-    return async (
-        _: IpcMainInvokeEvent,
-        props: IpcWindowCloseProps,
-    ): Promise<IpcResponseMap[typeof IPC_METHODS.WINDOW_CLOSE]> => {
-        try {
-            const closed = closeWindow({ parent: deps.parent, id: props.id })
-            if (!closed) {
-                return { ok: false, code: 'not_found', errorText: `window with id ${props.id} not found` }
-            }
-            return { ok: true, data: undefined }
-        } catch (error) {
-            console.error(error)
-            return { ok: false, code: 'error', errorText: 'Error while closing window' }
+    return wrapIpcHandler(IPC_METHODS.WINDOW_CLOSE, async (_, props: IpcWindowCloseProps) => {
+        const closed = closeWindow({ parent: deps.parent, id: props.id })
+        if (!closed) {
+            throw new IpcError('not_found', `window with id ${props.id} not found`)
         }
-    }
+    })
 }
