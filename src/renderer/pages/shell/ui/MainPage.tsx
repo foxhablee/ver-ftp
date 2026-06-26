700c5b3b36334a8a3a1cebebb408b605174a0157
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-26 11:43:40 +0300

    feat(shell): mount file manager in main page

--- src/renderer/pages/shell/ui/MainPage.tsx
+++ src/renderer/pages/shell/ui/MainPage.tsx
@@ -5,12 +5,15 @@ import Toolbar from './Toolbar'
 import { bookmarksTool } from '@/renderer/features/bookmarks-tool'
 import { connectionsTool } from '@/renderer/features/connections-tool'
 import { gitHistoryTool } from '@/renderer/features/git-history-tool'
+import { FileManager } from '@/renderer/features/file-manager'
 
 function MainPage(): React.JSX.Element {
     return (
         <Stack sx={{ height: '100%' }} direction='row'>
             <Toolbar side='left' tools={[bookmarksTool]} />
-            <Stack sx={{ width: '100%' }}></Stack>
+            <Stack sx={{ width: '100%' }}>
+                <FileManager />
+            </Stack>
             <Toolbar side='right' tools={[connectionsTool, gitHistoryTool]} />
         </Stack>
     )
