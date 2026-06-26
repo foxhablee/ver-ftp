9fcd7c4c7ff92dcc2a28f79da98b23f46ce9305e
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-26 13:36:26 +0300

    feat(file-manager): sticky panel and scrollable container

--- src/renderer/features/file-manager/ui/FileManager.tsx
+++ src/renderer/features/file-manager/ui/FileManager.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Stack, SxProps, Theme } from '@mui/material'
 import FileManagerProvider from './FileManagerProvider'
 import FileManagerList from './FileManagerList'
-import FileManagerPanel from '@/renderer/features/file-manager/ui/FileManagerPanel'
-import { Stack } from '@mui/material'
+import FileManagerPanel from './FileManagerPanel'
+
+const fileManagerSx = (theme: Theme) =>
+    ({
+        px: 1,
+        height: '100%',
+        overflow: 'auto',
+        width: '100%',
+        ':not(:first-of-type)': {
+            borderLeft: '1px solid',
+            borderLeftColor: theme.palette.divider,
+        },
+    }) as const satisfies SxProps<Theme>
 
 function FileManager(): React.JSX.Element {
     return (
         <FileManagerProvider connectionId={-1}>
-            <Stack sx={{ px: 1 }}>
+            <Stack sx={fileManagerSx}>
                 <FileManagerPanel />
                 <FileManagerList />
             </Stack>
