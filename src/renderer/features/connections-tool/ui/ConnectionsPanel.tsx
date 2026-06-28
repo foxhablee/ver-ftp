e20d27532a8e66534f3d6da4af3ce66f8b559767
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-28 14:26:51 +0300

    refactor(connections-tool): use 'useWindowApi' in ConnectionsPanel

--- src/renderer/features/connections-tool/ui/ConnectionsPanel.tsx
+++ src/renderer/features/connections-tool/ui/ConnectionsPanel.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Button } from '@mui/material'
-import { useLazyCreateWindowQuery } from '@/renderer/shared/api'
+import { useWindowApi } from '@/renderer/shared/lib'
 
 function ConnectionsPanel(): React.JSX.Element {
-    const [createWindow] = useLazyCreateWindowQuery()
-
-    function clickHandler(): void {
-        createWindow({ page: 'new-connection' })
-    }
+    const { openNewWindow } = useWindowApi()
 
     return (
         <>
-            <Button variant='outlined' type='button' onClick={clickHandler}>
+            <Button variant='outlined' type='button' onClick={() => openNewWindow('new-connection')}>
                 Создать подключение
             </Button>
         </>
