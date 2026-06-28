73f5c58ed6475f41c27433aca39f68aaac604cd9
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-28 22:25:10 +0300

    feat(shared/lib): isErrnoException and tryJson helpers

--- src/backend/shared/lib/index.ts
+++ src/backend/shared/lib/index.ts
@@ -1,2 +1,4 @@
 export { wrapIpcHandler } from './wrapIpcHandler'
 export { sortByKind } from './sort-fs-items'
+export { isErrnoException } from './is-error'
+export { tryJsonParse, tryJsonStringify } from './try-json'
